974b8cb01109d8414cecd92ee4072b4108cf3513
Petar Glazar <petar.glazar@gmail.com>
2017-11-23 15:41:51 +0100

    package skeleton

---
+++ R/cb2tools.R
@@ -0,0 +1,26 @@
+#' cb2tools: circBase2 management tools
+#'
+#' The foo package provides three categories of important functions:
+#' foo, bar and baz.
+#'
+#' @section Foo functions:
+#' The foo functions ...
+#'
+#' @docType package
+#' @name cb2tools
+#' @importFrom rstudioapi askForPassword
+# @importFrom data.table data.table
+# @import DBI
+# @import RMySQL
+NULL
+
+.onLoad <- function(libname, pkgname) {
+  op <- options()
+  op.cb2 <- list(
+    mysql.db   = "circBase2"
+  )
+  toset <- !(names(op.cb2) %in% names(op))
+  if (any(toset)) options(op.cb2[toset])
+
+  invisible()
+}

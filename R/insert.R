cf4a43727489bd5797499f8c133f45ecf914b6ca
Petar Glazar <petar.glazar@gmail.com>
2017-12-08 19:24:12 +0100

    all inserts working...

--- R/insert.R
+++ R/insert.R
@@ -42,9 +42,9 @@ dbDump <- function(conn, CS, CS.annot) {
                                  "WHERE circBaseID IN ",
                                    collapseVector(DT$circBaseID)))
 
-  res.DT <- data.table(fetch(res))
+  id2cbid <- data.table(fetch(res))
 
-  tmp.DT <- merge(DT, res.DT, by="circBaseID")
+  tmp.DT <- merge(DT, id2cbid, by="circBaseID")
   tmp.DT$spliced_length <- tmp.DT$end - tmp.DT$start # TODO: this is dummy shit
   tmp.DT <- tmp.DT[,.(circID, spliced_length, gene_id, feature, junct.known)]
   tmp.DT$best_transcript <- tmp.DT$gene_id
@@ -60,6 +60,85 @@ dbDump <- function(conn, CS, CS.annot) {
   insertStatement <- sqlAppendTable(conn, db.tables[3], tmp.DT)
   dbExecute(conn, insertStatement)
   rm(tmp.DT)
+
+  #############
+  ### study ###
+  #############
+  # dbListFields(conn, "study")
+  insertStatement <- sqlAppendTable(con   = conn,
+                                    table = "study",
+                                    values = CS.annot$study)
+  dbExecute(conn, insertStatement)
+
+  ###############
+  ### library ###
+  ###############
+  # dbListFields(conn, "library")
+  insertStatement <- sqlAppendTable(con = conn,
+                                    table = "library",
+                                    values = CS.annot$lib)
+  dbExecute(conn, insertStatement)
+
+  #####################
+  ### study_library ###
+  #####################
+  # will probably not need this in the future when the keys are set properly
+  refz <- data.table(study   = rep("ENCODE", length(colnames(circs.se))),
+                     library = colnames(circs.se))
+
+  res <- dbSendQuery(conn, paste0("SELECT studyID, name FROM study WHERE name IN ", collapseVector(refz$study)))
+  tmp <- fetch(res)
+
+  refz <- merge(refz, tmp, by.x = "study", by.y = "name")
+
+  res <- dbSendQuery(conn, paste0("SELECT libraryID, name FROM library WHERE name IN ", collapseVector(refz$library)))
+  tmp <- fetch(res)
+
+  refz <- merge(refz, tmp, by.x = "library", by.y = "name")
+
+  # ^this shit should be tested before INSERT
+  insertStatement <- sqlAppendTable(con    =   conn,
+                                    table  = "study_library",
+                                    values = refz[,.(studyID, libraryID)])
+  dbExecute(conn, insertStatement) # YOLO
+
+  ######################
+  ### X_Y_expression ###
+  ######################
+  #dbListFields(conn, "hsa_hg19_expression")
+
+  DT2 <- merge(DT, id2cbid, by="circBaseID")
+  tab.list <- lapply(CS.annot$lib$name, function(x) DT2[, c(1, grep(x, names(DT2))), with=F])
+  tab.list <- lapply(tab.list, setnames, c("circBaseID",
+                                           "n_reads_circ",
+                                           "n_reads_circ_uniq",
+                                           "n_reads_5pr",
+                                           "n_reads_3pr",
+                                           "ratio"))
+  tab.list <- lapply(tab.list, function(x) merge(x, DT2[,.(circBaseID, circID)], by="circBaseID"))
+  names(tab.list) <- CS.annot$lib$name
+
+  res <- dbSendQuery(conn, paste0("SELECT libraryID, name FROM library WHERE name IN ", collapseVector(names(tab.list))))
+  libid2libname <- data.table(fetch(res))
+
+
+  for (lib in names(tab.list)) {
+    # yay for loop
+    print(lib)
+    tab.list[[lib]]$libraryID <- libid2libname[name == lib]$libraryID
+    tab.list[[lib]]$n_reads_5pr_uniq <- NA
+    tab.list[[lib]]$n_reads_3pr_uniq <- NA
+
+    insertStatement <- sqlAppendTable(con   = conn,
+                                      table = db.tables[2],
+                                      tab.list[[lib]][,.(circID, libraryID,
+                                                         n_reads_circ, n_reads_5pr, n_reads_3pr,
+                                                         n_reads_circ_uniq, n_reads_5pr_uniq,
+                                                         n_reads_3pr_uniq)])
+    dbExecute(conn, insertStatement) # YOLO
+
+
+  }
 
 
 
--- inst/scripts/testDb.R
+++ inst/scripts/testDb.R
@@ -24,97 +24,15 @@ dbLinesPerTable(conn)
 
 CS.annot <- list(
   organism = "hsa",
-  assembly = "hg19"
+  assembly = "hg19",
+  study    = data.frame(name        = "ENCODE",
+                        description = "magic ENCODE stuff from the interwebz",
+                        reference   = "encodeproject.org"),
+  lib      = data.frame(name        = colnames(circs.se),
+                        description = paste(colnames(circs.se),  "desc"),
+                        reference   = paste0(colnames(circs.se), "_ref"))
 )
 
 dbDump(conn, circs.se, CS.annot)
 dbLinesPerTable(conn)
 dbDisconnect(conn)
-
-
-##################
-### study_name ###
-##################
-dbListFields(conn, "study")
-insertStatement <- sqlAppendTable(conn,
-                                  "study",
-                                  data.frame(name        = "ENCODE",
-                                             description = "magic ENCODE stuff from the interwebz",
-                                             reference   = "www.encodeproject.org"))
-dbExecute(conn, insertStatement)
-
-####################
-### library_name ###
-####################
-libz <- data.table(name        = colnames(circs.se),
-                   description = paste(colnames(circs.se),  "desc"),
-                   reference   = paste0(colnames(circs.se), "_ref"))
-dbListFields(conn, "library")
-insertStatement <- sqlAppendTable(conn,
-                                  "library",
-                                  libz)
-dbExecute(conn, insertStatement)
-
-#####################
-### study_library ###
-#####################
-refz <- data.table(study   = rep("ENCODE", length(colnames(circs.se))),
-                   library = colnames(circs.se))
-
-
-res <- dbSendQuery(conn, paste0("SELECT studyID, name FROM study WHERE name IN ", collapseVector(refz$study)))
-tmp <- fetch(res)
-
-refz <- merge(refz, tmp, by.x = "study", by.y = "name")
-
-res <- dbSendQuery(conn, paste0("SELECT libraryID, name FROM library WHERE name IN ", collapseVector(refz$library)))
-tmp <- fetch(res)
-
-refz <- merge(refz, tmp, by.x = "library", by.y = "name")
-
-# this shit should be tested before INSERT
-insertStatement <- sqlAppendTable(conn,
-                                  "study_library",
-                                  refz[,.(studyID, libraryID)])
-dbExecute(conn, insertStatement) # YOLO
-
-
-
-
-
-###########################
-### hsa_hg19_expression ###
-###########################
-dbListFields(conn, "hsa_hg19_expression")
-
-tab2 <- merge(tab, chunk, by="circBaseID")
-tab.list <- lapply(refz$library, function(x) tab2[, c(1, grep(x, names(tab2))), with=F])
-tab.list <- lapply(tab.list, setnames, c("circBaseID",
-                                         "n_reads_circ",
-                                         "n_reads_circ_uniq",
-                                         "n_reads_5pr",
-                                         "n_reads_3pr",
-                                         "ratio"))
-tab.list <- lapply(tab.list, function(x) merge(x, tab2[,.(circBaseID, circID)], by="circBaseID"))
-names(tab.list) <- refz$library
-
-for (lib in names(tab.list)) {
-  # yay for loop
-  print(lib)
-  tab.list[[lib]]$libraryID <- refz[library == lib]$libraryID
-  tab.list[[lib]]$n_reads_5pr_uniq <- NA
-  tab.list[[lib]]$n_reads_3pr_uniq <- NA
-
-  insertStatement <- sqlAppendTable(conn,
-                                    "hsa_hg19_expression",
-                                    tab.list[[lib]][,.(circID, libraryID,
-                                                       n_reads_circ, n_reads_5pr, n_reads_3pr,
-                                                       n_reads_circ_uniq, n_reads_5pr_uniq,
-                                                       n_reads_3pr_uniq)])
-  dbExecute(conn, insertStatement) # YOLO
-
-
-}
-
-dbDisconnect(conn)
-
